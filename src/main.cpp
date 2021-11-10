6b862d8e9574513adccd9dd1193389a61a6b411a
Lets-A-Code <74007285+Lets-A-Code@users.noreply.github.com>
2021-11-10 21:49:25 +0600

    added keyboard input

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,34 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-using namespace sf;
-using namespace std;
-
 int main() {
-    RenderWindow window(VideoMode(1280, 720), "Game");
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Game");
 
     bool up, down, left, right;
 
     while (window.isOpen()) {
-        Event event;
+        sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == Event::Closed)
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        if(Keyboard::isKeyPressed(Keyboard::W) || Keyboard::isKeyPressed(Keyboard::Up)) up = true;
-        if(!(Keyboard::isKeyPressed(Keyboard::W) || Keyboard::isKeyPressed(Keyboard::Up))) up = false;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) up = true;
+        if(!(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))) up = false;
 
-        if(Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down)) down = true;
-        if(!(Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down))) down = false;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) down = true;
+        if(!(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))) down = false;
 
-        if(Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left)) left = true;
-        if(!(Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left))) left = false;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) left = true;
+        if(!(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))) left = false;
 
-        if(Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right)) right = true;
-        if(!(Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right))) right = false;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) right = true;
+        if(!(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))) right = false;
 
-        cout << "up: " << up << " down: " << down << " left: " << left << " right: " << right << endl;
+        std::cout << "up: " << up << " down: " << down << " left: " << left << " right: " << right << std::endl;
 
-        window.clear(Color(50, 50, 50));
+        window.clear(sf::Color(50, 50, 50));
+        window.setFramerateLimit(60);
         window.display();
     }
 
