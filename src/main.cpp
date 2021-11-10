485fc64591822beec141dbd369b04361b5701967
Lets-A-Code <74007285+Lets-A-Code@users.noreply.github.com>
2021-11-10 19:30:23 +0600

    added window

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
+#include <SFML/Graphics.hpp>
 
+using namespace sf;
+
+int main() {
+    RenderWindow window(VideoMode(1280, 720), "Game");
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear(Color(50, 50, 50));
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
