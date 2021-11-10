1ffcf8947aeb93208f9f627d39aa7f113ef6e36a
Lets-A-Code <74007285+Lets-A-Code@users.noreply.github.com>
2021-11-10 21:48:47 +0600

    added keyboard input

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
+using namespace std;
 
 int main() {
     RenderWindow window(VideoMode(1280, 720), "Game");
+
+    bool up, down, left, right;
 
     while (window.isOpen()) {
         Event event;
@@ -11,6 +15,20 @@ int main() {
             if (event.type == Event::Closed)
                 window.close();
         }
+
+        if(Keyboard::isKeyPressed(Keyboard::W) || Keyboard::isKeyPressed(Keyboard::Up)) up = true;
+        if(!(Keyboard::isKeyPressed(Keyboard::W) || Keyboard::isKeyPressed(Keyboard::Up))) up = false;
+
+        if(Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down)) down = true;
+        if(!(Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down))) down = false;
+
+        if(Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left)) left = true;
+        if(!(Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left))) left = false;
+
+        if(Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right)) right = true;
+        if(!(Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right))) right = false;
+
+        cout << "up: " << up << " down: " << down << " left: " << left << " right: " << right << endl;
 
         window.clear(Color(50, 50, 50));
         window.display();
